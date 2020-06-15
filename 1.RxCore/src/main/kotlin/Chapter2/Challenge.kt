2a9fc2f34d252792a26465458a80f2279b91900b
Igor Novikov <novikov9@yahoo.com>
2020-06-15 14:18:17 +0300

    chapter 3

---
+++ 1.RxCore/src/main/kotlin/Chapter2/Challenge.kt
@@ -0,0 +1,25 @@
+package Chapter2
+
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+
+fun main(){
+    exampleOf("never") {
+        val disposables = CompositeDisposable()
+        val observable = Observable.never<Any>()
+        //Полезные методы, которые можно вызывать в момент завершения, подписки и очистки
+        val subscription = observable
+                .doOnNext { println(it) }
+                .doOnComplete { println("Completed") }
+                .doOnSubscribe { println("Subscribed") }
+                .doOnDispose { println("Disposed") }
+                .subscribeBy(onNext = {
+                    println(it)
+                }, onComplete = {
+                    println("Completed")
+                })
+        disposables.add(subscription)
+        disposables.dispose()
+    }
+}
--- 1.RxCore/src/main/kotlin/SupportCode.kt
+++
@@ -1,5 +0,0 @@
-
-fun exampleOf(description: String, action: () -> Unit) {
-    println("\n--- Example of: $description ---")
-    action()
-}
