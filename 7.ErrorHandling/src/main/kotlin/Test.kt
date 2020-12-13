0e5fe86cd5e3aa062528fa18331b35bdd5bdb4ff
Igor Novikov <novikov.mail@inbox.ru>
2020-12-13 15:39:59 +0300

    save point

--- 7.ErrorHandling/src/main/kotlin/Test.kt
+++ 7.ErrorHandling/src/main/kotlin/Test.kt
@@ -1,40 +1,40 @@
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import java.lang.Exception
+import java.lang.IllegalArgumentException
 
 fun main(){
-    rxFunction()
-        .retry(1)
-    .subscribe()
+    val compositeDisposable = CompositeDisposable()
+
+    try {
+        compositeDisposable.add(rxFunction()
+            .retry(2)
+            .onErrorReturn { throw IllegalArgumentException("Som") }
+            .subscribe({ println(it)},{
+                it.printStackTrace()
+            }))
+    }
+    catch (ex:Exception){
+        ex.printStackTrace()
+    }
 }
 
 object Switch{
-    var switch = false
+    var switch = true
 }
 
 fun rxFunction(): Observable<String> {
-    if (Switch.switch){
-        return Observable.create<String> { emitter ->
-            emitter.onNext("1")
-            emitter.onNext("2")
-            emitter.onNext("3")
-            emitter.onComplete()
-        }.doOnNext { println(it)}
-            .doOnError {
-                println("onError correct")
-                println(Switch.switch)
-                Switch.switch = true }
-    }
-    else {
-        return Observable.create<String> { emitter ->
-            emitter.onError(throw Exception("Hello my friend!"))
-            emitter.onNext("1")
-            emitter.onNext("2")
-            emitter.onNext("3")
-            emitter.onComplete()
+    return Observable.create<String> { emitter ->
+        if (Switch.switch) {
+            emitter.onError(throw Exception("Hello, Friend!"))
+        }
+        emitter.onNext("1")
+        emitter.onNext("2")
+        emitter.onNext("3")
+        emitter.onComplete()
     }.doOnNext { println(it) }
-            .doOnError {
-                println("onError wrong")
-                println(Switch.switch)
-                Switch.switch = true }
-    }
+        .doOnError {
+            println("now Switch " + Switch.switch)
+//                Switch.switch = false }
+        }
 }
