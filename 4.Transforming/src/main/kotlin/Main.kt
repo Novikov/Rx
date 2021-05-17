c615156afe7600be943b2f031ad6e074df8e6ffc
Igor Novikov <novikov.mail@inbox.ru>
2021-05-17 13:17:05 +0300

    transforming

--- 4.Transforming/src/main/kotlin/Main.kt
+++ 4.Transforming/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
 
@@ -40,16 +41,41 @@ fun main(args: Array<String>) {
                         })
     }
 
+
+    //На каждый элемент Observable выпускает 0, 1 или несколько элементов другого Observable, а затем сглаживает всё в один Observable
+    //Происходит подмена одного Observable на другой
     exampleOf("flatMap") {
         val observable = Observable
                 .just("A", "B", "C")
-                .switchMap { s: String ->
-                    println()
+                .flatMap { s: String ->
                     Observable.just(s + "1", s + "2", s + "3")
                 }
 
         observable.subscribe { s: String -> print("$s ") }
     }
+
+    //Тоже самое, что и flatMap, только гарантируется порядок элементов
+    exampleOf("concatMap") {
+        val observable = Observable
+                .just("A", "B", "C")
+                .concatMap { s: String ->
+                    Observable.just(s + "1", s + "2", s + "3")
+                }
+
+        observable.subscribe { s: String -> print("$s ") }
+    }
+
+    exampleOf("switchMap") {
+        val observable = Observable
+                .just("A", "B", "C")
+                .switchMap { s: String ->
+                    Observable.just(s + "1", s + "2", s + "3").delay(1, TimeUnit.MILLISECONDS)
+                }
+
+        observable.subscribe { s: String -> print("$s ") }
+    }
+
+
 
 
 }
